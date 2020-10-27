610560149d3a6573a0c5cde9a5d787231ce62c03
rf-spuds <rfspuds@gmail.com>
2020-10-27 15:01:43 -0400

    update to directory structure; removed old files

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,58 +6,75 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 // ! Model Creation template
-const User = mongoose.model("User", {
-    name: {
-        type: String,
-        required: true,
-        trim : true
-    },
-    email:{
-        type: String,
-        required : true,
-        trim : true, 
-        lowercase : true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error ("That's not a valid email.")
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default:0,
-        validate(value) {
-            if(value <0 ){
-                throw new Error ("Age must be a positive number")
-            }
-        }
-    }
-})
-
-const me = new User({
-    name: "Reggie                                                                   ",
-    email: "REGGIE@gmail.com",
-})
-
-me.save().then((me)=>{console.log(me)}).catch((e)=> console.log("Error!", e))
-
-// const Task = mongoose.model('Task', 
-// {
-//     task: {
-//         type: String
+// const User = mongoose.model("User", {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim : true
 //     },
-//     completed: {
-//         type: Boolean
+//     email:{
+//         type: String,
+//         required : true,
+//         trim : true, 
+//         lowercase : true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error ("That's not a valid email.")
+//             }
+//         }
+//     },
+//     password:{
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value){
+//             if(validator.contains(value, "password", {ignoreCase: true})){
+//                 throw new Error ("Password can't be password")
+//             }
+//         }
+
+//     },
+//     age: {
+//         type: Number,
+//         default:0,
+//         validate(value) {
+//             if(value <0 ){
+//                 throw new Error ("Age must be a positive number")
+//             }
+//         }
 //     }
-// }
-// )
+// })
 
-// const newTask = new Task(
-//     {
-//         task : "Clean toilet",
-//         completed : false
-//     }
+// const me = new User({
+//     name: "Reggie                                                                   ",
+//     email: "REGGIE@gmail.com",
+//     password : "zJyXkVjbAjsNy697sqBKzJyXkVjbAjsNy697sqBKzJyXkVjbAjsNy697sqBKzJyXkVjbAjsNy697sqBKzJyXkVjbAjsNy697sqBKzJyXkVjbAjsNy697sqBK",
+//     age: 45
+// })
 
-// )
+// me.save().then((me)=>{console.log(me)}).catch((e)=> console.log("Error!", e))
 
-// newTask.save().then((newTask) => console.log(newTask)).catch((e)=>console.log("Error!" + e))
+const Task = mongoose.model('Task', 
+{
+    description: {
+        type: String,
+        trim : true,
+        required : true
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+        required : false
+    }
+}
+)
+
+const newTask = new Task(
+    {
+        description : "Clean toilet"
+    }
+
+)
+
+newTask.save().then((newTask) => console.log(newTask)).catch((e)=>console.log("Error!" + e))
