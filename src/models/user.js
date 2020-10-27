03d3c67dcad16de821c770518dcdac7831ff8afb
rf-spuds <rfspuds@gmail.com>
2020-10-27 15:30:57 -0400

    models created

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("That's not a valid email.");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (validator.contains(value, "password", { ignoreCase: true })) {
+        throw new Error("Password can't be password");
+      }
+    },
+    validate(value) {
+      if (value.length > 128) {
+        throw new Error("Password is too long");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
+
+module.exports = User;
